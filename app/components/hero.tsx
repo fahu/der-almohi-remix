698521049820f170329674ec11d13c9010b02ced
Fabian Hurnaus <fahu@outlook.com>
2022-05-28 18:35:25 +0200

    Adjust link to header image
    The header image has been moved to a different folder in Cloudinary.

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,6 +1,6 @@
 export const HeroComponent = () => {
   return (
-    <header className="flex h-128 w-full flex-col rounded-xl bg-gray-300 bg-[url('https://res.cloudinary.com/dbsjhw9tp/image/upload/c_scale,w_1488/v1650748932/der-almohi-hero-schneiderberg-gruenau-2021_i1s7vp.jpg')] bg-cover bg-center p-8 bg-blend-multiply">
+    <header className="flex h-128 w-full flex-col rounded-xl bg-gray-300 bg-[url('https://res.cloudinary.com/dbsjhw9tp/image/upload/c_scale,w_1488/v1650748932/der-almohi/der-almohi-hero-schneiderberg-gruenau-2021_i1s7vp.jpg')] bg-cover bg-center p-8 bg-blend-multiply">
       <div className="my-auto ml-12">
         <h1 className="order-1 text-6xl font-medium text-white">
           {" "}
