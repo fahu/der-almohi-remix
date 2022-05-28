a7e963b776beecf0fc9104d0b2d8dc131ad04fff
Fabian Hurnaus <fahu@outlook.com>
2022-05-28 19:01:07 +0200

    Add a title to the website

--- app/root.tsx
+++ app/root.tsx
@@ -29,7 +29,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Der Almöhi - Hikes and nice landscapes for everyone 🏔",
   viewport: "width=device-width,initial-scale=1",
 });
 
