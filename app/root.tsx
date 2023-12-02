c8a15cfa9bd06a9230869359d34b9aa653ed65b3
Fabian Hurnaus <fabian.hurnaus@tractive.com>
2023-12-02 23:47:49 +0100

    Set favicon

--- app/root.tsx
+++ app/root.tsx
@@ -17,6 +17,7 @@ import tailwindStylesheetUrl from "./styles/tailwind.css";
 
 export const links: LinksFunction = () => {
   return [
+    { rel: "icon", href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥾</text></svg>"},
     { rel: "stylesheet", href: tailwindStylesheetUrl },
     { rel: "preconnect", href: "https://res.cloudinary.com" },
     {
